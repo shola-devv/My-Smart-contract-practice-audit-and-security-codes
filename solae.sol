fdc5e5c7447bdfb428730f56b449fa2418bcfdfc
shola emmanuel <creekemmanuelf@gmail.com>
2024-09-04 00:25:49 +0100

    a sexy commit

--- solae.sol
+++ solae.sol
@@ -15,3 +15,39 @@ contract myFaucet{
     fallback() external payable { } 
     
 }
+
+//learning about contract that extends other functions
+contract first is myFaucet{
+   adress owner;
+
+   contructor shales{
+    require(owner == msg.sender);
+
+   }
+   function sendFunds()public shales{
+        
+   }
+
+}
+//estimating gas price
+var contract = web3.eth.contract(abi).at(address);
+var gasEstimate = contract.myAweSomeMethod.estimateGas(arg1, arg2,
+ {from: account})
+
+contract caller {
+18 
+19 function make_calls(calledContract _calledContract) public {
+20 
+21 // Calling calledContract and calledLibrary directly
+22 _calledContract.calledFunction();
+23 calledLibrary.calledFunction();
+24 
+25 // Low-level calls using the address object for calledContract
+26 require(address(_calledContract).
+27 call(bytes4(keccak256("calledFunction()"))));
+28 require(address(_calledContract).
+29 delegatecall(bytes4(keccak256("calledFunction()"))));
+30 git commit -m"a sexy commit"
+31 
+32 
+33 
