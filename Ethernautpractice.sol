fbb7525cfc8669fd06dfdde432cf4545db87930d
shola emmanuel <creekemmanuelf@gmail.com>
2024-09-10 17:34:14 +0100

    som esolidity practice

--- Ethernautpractice.sol
+++ Ethernautpractice.sol
@@ -67,4 +67,45 @@ contract CoinFlip{
 }
 //so im supposed to deploy the contract and win 10 consecutive ConsecutiveWins
 //solution- calculate the blockhash function using the block.number then confirm the bool;
- 
+  
+ // ETHERNAUT CHALLENGE 4 -TELLEPHONE
+
+ contract Telephone{
+ address pubblic Owner;
+ address public x;
+ constructor()public{
+    owner = msg.sender;
+    x = message.sender;
+ }
+
+function ChangeOwner(address Owner) public{
+    require(tx.origin == owner, "owner can change owner");
+    owner = _owner
+}
+ }
+//so im deploy and set owner
+///hahahaha, its obvious,just deploy and call the function with the address
+
+
+// ETHERNAUT CHALLENGE 5- TOLKIEN, lol token
+
+contract Tolken {
+  mappping(address => uint256)public balances;
+  uint256 publictotalSupply;
+constructir()public{
+    mapping (address => uint256)public balances;
+    uint 256 public totalSupply
+}
+
+function transfer(address to, uint256 _value)public{
+    require(balances[msg.sender] >= _value, "insufficient balance");
+      balances[msg.sender] -= _value;
+      balances[to] += _value;
+}
+function balanceOf(address _owner) public view returns(uint256){
+return balances[_owner];
+}
+}
+
+//so im to deploy and dran the contracts funds
+// justt deploy then call the transfer function with a huge ammount of money
